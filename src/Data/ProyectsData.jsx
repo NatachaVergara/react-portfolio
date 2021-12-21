3d86c54f97b0d66e295580a1fd12ef3e4750bbfb
Natacha Vergara <ntchvergara@gmail.com>
2021-12-21 11:15:14 -0300

    agregando seccion portfolio/portfolioContainer/proyectsData

---
+++ src/Data/ProyectsData.jsx
@@ -0,0 +1,133 @@
+export const ProyectsData = [
+    {
+        title:'Potfolio Personal',
+        link: 'https://nvergaraportfolio.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'animate css','font-awsome', 'google-fonts', 'netlify' ]
+    },
+    {
+        title: 'Mis primeros pasos',
+        link: 'https://mishtmls.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'google-fonts', 'netlify']
+    },
+
+    {
+        title: 'Pequeños proyectos con Javascrip',
+        link: 'https://proyectosdejs.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'animate css', 'font-awsome', 'google-fonts', 'javascript', 'netlify']
+    },
+    
+    {
+        title: 'Piedra, Papel o Tijera',
+        link: 'https://juegojs.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'javascript', 'netlify']
+    },
+    {
+        title: 'Una experiencia coreana',
+        link: 'https://unaexperienciacoreana.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'javascript', 'netlify']
+    },
+    {
+        title: 'Recetas para celíacos (en construción )',
+        link: 'https://resetasceliaquia.herokuapp.com/index',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'javascript', 'Heroku', 'node', 'api']
+    },
+    {
+        title: 'Canal del clima',
+        link: 'https://canaldelclimacongit.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'javascript', 'api']
+    }, 
+    {
+        title: 'Cuenta regresiva',
+        link: 'https://cuentaregresiva-eventos.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'google-fonts', 'javascript', 'localStorage']
+    },
+    {
+        title: 'Busco libro App - React',
+        link: 'https://busco-libro-app.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'bootstrap', 'font-awsome', 'javascript', 'React', 'react-router-dom','react-bootstrap', 'Firebase']
+    }, 
+    {
+        title: 'Find your recipe',
+        link: 'https://find-your-recipe-nv.netlify.app/',
+        img: '',
+        tecnologies: ['html', 'css', 'javascript', 'bootstrap', 'React', 'Api']
+    }
+    // , 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+    // {
+    //     title: '',
+    //     link: '',
+    //     img: '',
+    //     tecnologies: ['', '', '', '', '']
+    // }, 
+
+
+]
--- src/RouterApp/RouterApp.jsx
+++ src/RouterApp/RouterApp.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import HomeContainer from '../components/Home/HomeContainer';
+import PortfolioContainer from '../components/Portfolio/PortfolioContainer';
 
 
 
 const RouterApp = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomeContainer  />} />            
+            <Route path="/" element={<HomeContainer />} />  
+            <Route path="/portfolio" element={<PortfolioContainer />} />  
+            
         </Routes>
     )
 }
