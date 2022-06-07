660523c74b85c56af1a32be15ef481e29cffd41c
Natacha Vergara <ntchvergara@gmail.com>
2022-06-07 15:00:13 -0300

    la carga de imagenes no funciona, viendo el porque

--- src/components/Panel_Control/SkillSectionHome/AddSkill.js
+++ src/components/Panel_Control/SkillSectionHome/AddSkill.js
@@ -1,21 +1,63 @@
+import axios from 'axios'
+import { Form, Formik } from 'formik'
 import React from 'react'
 import { AiFillFileAdd } from 'react-icons/ai'
+import { BASE_URL } from '../../../Utils/URL'
 import MenuCPanel from '../MenuCPanel/MenuCPanel'
-
+import { ImageInput } from "formik-file-and-image-input/lib";
 const AddSkill = () => {
+    const imageFormats = ["image/png", "image/svg", "image/jpeg"];
+
+    const uploadImg = (values) => {
+        axios.post(`${BASE_URL}/images/upload`, values, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err))
+    }
 
 
 
-    
     return (
         <>
             <MenuCPanel linktTo='/controlPanel/homeskills' name='Skills' />
             <div className='form'>
-                <form>
-                    <label htmlFor='file'  className='fs-1'><AiFillFileAdd /></label>
-                    <input type='file' name='skill' required autoComplete='off' multiple />
+
+                <Formik
+                    initialValues={{
+                        images: null
+                    }}
+                    onSubmit={(values) => {
+                        console.log(values)
+                        uploadImg(values)
+                    }}
+
+                >
+                    <Form>
+                        <div className='d-flex flex-column justify-content-center align-items-center'>
+                            <ImageInput
+
+                            name="images"
+                            validFormats={imageFormats}
+                        />
+                            <button type='submit' className='btn btn-outline-success text-dark  img-button'>Agregar</button></div>
+
+                    </Form>
+
+
+
+
+                </Formik>
+
+
+
+                {/* <form>
+                    <label htmlFor='file' className='fs-1'><AiFillFileAdd /></label>
+                    <input type='file' name='images' required autoComplete='off' multiple />
                     <button className='btn btn-outline-success text-dark  img-button'>Agregar </button>
-                </form>
+                </form> */}
             </div>
 
 
