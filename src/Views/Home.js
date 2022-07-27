11aee7e2057e8ade15f5599367a02a2d9b078aac
Natacha Vergara <ntchvergara@gmail.com>
2022-07-27 16:51:25 -0300

    creando nuevo dashboard

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import HomeContainer from '../components/Home/HomeContainer'
 
+
 const Home = () => {
   return (
     <>
-      <HomeContainer/>
+     
+      <HomeContainer />
     </>
   )
 }
