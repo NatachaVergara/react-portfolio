7431722c9fd75ef8b938c0fda3baa4f87a3ff322
Natacha Vergara <ntchvergara@gmail.com>
2022-10-17 12:12:03 -0300

    actualice el context

--- src/Store/useContext.js
+++ src/Store/useContext.js
@@ -1,336 +1,330 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_URL } from "../Utils/URL";
+import Swal from "sweetalert2";
+import { agregado } from "../components/sweetAlerts/alert";
+import Cookies from "js-cookie";
 
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import axios from 'axios'
-import { BASE_URL } from '../Utils/URL'
-import Swal from 'sweetalert2'
-import { agregado } from '../components/sweetAlerts/alert'
-import Cookies from 'js-cookie'
-
-
-
-const UserContext = createContext()
+const UserContext = createContext();
 
 export const useUserContext = () => {
-    return useContext(UserContext)
-}
-
-
-
-
+  return useContext(UserContext);
+};
 
 const UserContextProvider = ({ children }) => {
-    const [userId, setUserId] = useState(Cookies.get('userId') ? JSON.parse(Cookies.get('userId')) : '')
-    const [isUser, setIsUser] = useState(Cookies.get('isUser') ? JSON.parse(Cookies.get('isUser')) : null)
-    const [userType, setUserType] = useState(Cookies.get('userType') ? JSON.parse(Cookies.get('userType')) : '')
-    const [proyects, setProyects] = useState(Cookies.get('proyects') ? JSON.parse(Cookies.get('proyects')) : [])
-    const [imagenes, setImagenes] = useState(Cookies.get('imagenes') ? JSON.parse(Cookies.get('imagenes')) : [])
-    const [loading, setLoading] = useState(false)
-    const [imgsSlider, setImgsSlider] = useState(Cookies.get('imgsSlider') ? JSON.parse(Cookies.get('imgsSlider')) : [])
-    const [about, setAbout] = useState(Cookies.get('about') ? JSON.parse(Cookies.get('about')) : [])
-    const [precios, setPrecios] = useState([])
+  const [userId, setUserId] = useState(
+    Cookies.get("userId") ? JSON.parse(Cookies.get("userId")) : ""
+  );
+  const [isUser, setIsUser] = useState(
+    Cookies.get("isUser") ? JSON.parse(Cookies.get("isUser")) : null
+  );
+  const [userType, setUserType] = useState(
+    Cookies.get("userType") ? JSON.parse(Cookies.get("userType")) : ""
+  );
+  const [proyects, setProyects] = useState(
+    Cookies.get("proyects") ? JSON.parse(Cookies.get("proyects")) : []
+  );
+  const [imagenes, setImagenes] = useState(
+    Cookies.get("imagenes") ? JSON.parse(Cookies.get("imagenes")) : []
+  );
+  const [loading, setLoading] = useState(false);
 
+  const [imgsSlider, setImgsSlider] = useState(
+    Cookies.get("imgsSlider") ? JSON.parse(Cookies.get("imgsSlider")) : []
+  );
+  const [about, setAbout] = useState(
+    Cookies.get("about") ? JSON.parse(Cookies.get("about")) : []
+  );
+  const [precios, setPrecios] = useState(
+    Cookies.get("precios") ? JSON.parse(Cookies.get("precios")) : []
+  );
 
+ 
 
+  const logOut = () => {
+    setUserId(null);
+    setIsUser(null);
+    setUserType('');
+    Swal.fire("Se ha deslogueado exitosamente");
+  };
+  //Imagenes de skills
+  const uploadData = async () => {
+    try {
+      const responseSkills = await axios.get(`${BASE_URL}/upload/images`);
+      const responseSliders = await axios.get(`${BASE_URL}/upload/sliders`);
+      const responseProyects = await axios.get(`${BASE_URL}/proyects`);
+      const responseAbout = await axios.get(`${BASE_URL}/aboutme`);
+      const responsePrecios = await axios.get(`${BASE_URL}/precios`);
 
-    useEffect(() => {
-        Cookies.set('userIDSS', JSON.stringify(userId))
-        Cookies.set('isUserSS', JSON.stringify(isUser))
-        Cookies.set('userTypeSS', JSON.stringify(userType))
-        Cookies.set('skillImg', JSON.stringify(imagenes))
-        Cookies.set('carousel', JSON.stringify(imgsSlider))
-        Cookies.set('aboutMe', JSON.stringify(about))
-        Cookies.set('proyectsLS', JSON.stringify(proyects))
+      const imgsSkills = await responseSkills.data;
+      const sliders = await responseSliders.data;
+      const proyects = await responseProyects.data;
+      const aboutMe = await responseAbout.data;
+      const precios = await responsePrecios.data;
 
-    }, [userId, isUser, userType, imagenes, imgsSlider, about, proyects])
+     
+      setImagenes(imgsSkills);
+      setImgsSlider(sliders);
+      setAbout(aboutMe);
+      setPrecios(precios);
 
-
-
-    const logOut = () => {
-        setUserId(null)
-        setIsUser(null)
-        Swal.fire('Se ha deslogueado exitosamente')
-
+      if (proyects.length > 0) {
+        setLoading(false);
+        setProyects(proyects);
+      } else {
+        setLoading(true);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    //Imagenes de skills
-    const uploadData = async () => {
-        try {
-            const responseSkills = await axios.get(`${BASE_URL}/upload/images`)
-            const responseSliders = await axios.get(`${BASE_URL}/upload/sliders`)
-            const responseProyects = await axios.get(`${BASE_URL}/proyects`)
-            const responseAbout = await axios.get(`${BASE_URL}/aboutme`)
-            const responsePrecios = await axios.get(`${BASE_URL}/precios`)
+  };
+
+  useEffect(() => {
+    uploadData();
+  }, []);
 
 
-            const imgsSkills = await responseSkills.data
-            const sliders = await responseSliders.data
-            const proyects = await responseProyects.data
-            const aboutMe = await responseAbout.data
-            const precios = await responsePrecios.data
+  useEffect(() => {
+    Cookies.set("userId", JSON.stringify(userId), { expires: 7 });
+    Cookies.set("isUser", JSON.stringify(isUser), { expires: 7 });
+    Cookies.set("userType", JSON.stringify(userType), { expires: 7 });
+    Cookies.set("skillImg", JSON.stringify(imagenes), { expires: 365 });
+    Cookies.set("carousel", JSON.stringify(imgsSlider), { expires: 365 });
+    Cookies.set("aboutMe", JSON.stringify(about), { expires: 365 });
+    Cookies.set("proyects", JSON.stringify(proyects), { expires: 365 });
+    Cookies.set("precios", JSON.stringify(precios), { expires: 365 });
+  }, [userId, isUser, userType, imagenes, imgsSlider, about, proyects]);
 
-            console.log(imgsSkills)
-            setImagenes(imgsSkills)
-            setImgsSlider(sliders)
-            setAbout(aboutMe)
-            setPrecios(precios)
 
+  const [img, setImg] = useState(null);
 
-            if (proyects.length > 0) {
-                setLoading(false)
-                setProyects(proyects)
-            } else {
-                setLoading(true)
-            }
-
-        } catch (error) {
-            console.log(error)
-        }
+  const handleImg = (e) => {
+    const imagen = e.target.files[0];
+    if (imagen) {
+      setImg(imagen);
     }
+  };
 
-    useEffect(() => {
-        uploadData()
-    }, [])
+  const createProyect = async (values) => {
+    const { title, link, logo, tec } = values;
+    const formData = new FormData();
 
+    formData.append("image", img);
+    formData.append("title", title);
+    formData.append("link", link);
+    formData.append("logo", logo);
+    formData.append("tec", tec);
 
-
-
-    const [img, setImg] = useState(null)
-
-    const handleImg = (e) => {
-        const imagen = e.target.files[0]
-        if (imagen) {
-            setImg(imagen)
-        }
+    try {
+      const response = await axios.post(`${BASE_URL}/proyects`, formData, {
+        headers: { "content-type": "multipart/form-data" },
+      });
+      const proyectos = await response.data.registros;
+      const msg = await response.data.message;
+      if (response.status === 201) {
+        setProyects(proyectos);
+        console.log(msg);
+      } else {
+        console.log("No se ha creado el proyecto");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const createProyect = async (values) => {
-        const { title, link, logo, tec } = values
-        const formData = new FormData()
+  const deleteProyectbyId = async (path) => {
+    try {
+      const response = await axios.delete(`${BASE_URL}/proyects/${path}`);
+      const proyectos = await response.data.registros;
+      const msg = await response.data.message;
 
-        formData.append('image', img)
-        formData.append('title', title)
-        formData.append('link', link)
-        formData.append('logo', logo)
-        formData.append('tec', tec)
-
-        try {
-            const response = await axios.post(`${BASE_URL}/proyects`, formData, {
-                headers: { 'content-type': 'multipart/form-data' }
-            })
-            const proyectos = await response.data.registros
-            const msg = await response.data.message
-            if (response.status === 201) {
-                setProyects(proyectos)
-                console.log(msg)
-            } else {
-                console.log('No se ha creado el proyecto')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+      if (response.status === 200) {
+        setProyects(proyectos);
+        console.log(msg);
+      } else {
+        console.log("No se ha podido eliminar el archivo");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  //Imaganes de Skills
+  const uploadImg = async (values) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/upload`, values, {
+        headers: { "content-type": "multipart/form-data" },
+      });
+      const img = await response.data.registros;
+      const msg = await response.data.message;
+      console.log(response);
+      // console.log(msg)
 
-    const deleteProyectbyId = async (path) => {
-        try {
-            const response = await axios.delete(`${BASE_URL}/proyects/${path}`)
-            const proyectos = await response.data.registros
-            const msg = await response.data.message
-
-            if (response.status === 200) {
-                setProyects(proyectos)
-                console.log(msg)
-            }
-            else {
-                console.log('No se ha podido eliminar el archivo')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+      if (response.status === 201) {
+        console.log(msg);
+        setImagenes(img);
+        agregado(msg);
+      } else {
+        console.log("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const updateImg = async (file, path) => {
+   // console.log(file, path);
 
+    try {
+      const response = await axios.put(`${BASE_URL}/upload/${path}`, file, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+      const img = await response.data.registros;
+      const msg = await response.data.message;
+      console.log(img);
+      console.log(msg);
 
-
-    //Imaganes de Skills
-    const uploadImg = async values => {
-        try {
-            const response = await axios.post(`${BASE_URL}/upload`, values, {
-                headers: { 'content-type': 'multipart/form-data' }
-            })
-            const img = await response.data.registros
-            const msg = await response.data.message
-            console.log(response)
-            // console.log(msg)
-
-            if (response.status === 201) {
-                console.log(msg)
-                setImagenes(img)
-                agregado(msg)
-            } else {
-                console.log('Ha ocurrido un error')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-
+      if (response.status === 200) {
+        console.log(msg);
+        setImagenes(img);
+      } else {
+        console.log("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
-    const updateImg = async (file, path) => {
-        console.log(file, path)
+  };
 
-        try {
-            const response = await axios.put(`${BASE_URL}/upload/${path}`, file, { headers: { 'Content-Type': 'multipart/form-data' } })
-            const img = await response.data.registros
-            const msg = await response.data.message
-            console.log(img)
-            console.log(msg)
-
-            if (response.status === 200) {
-                console.log(msg)
-                setImagenes(img)
-            } else {
-                console.log('Ha ocurrido un error')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const deleteImg = async (path) => {
+    try {
+      const response = await axios.delete(`${BASE_URL}/upload/${path}`);
+      const img = await response.data.registros;
+      const msg = await response.data.message;
+      if (response.status === 200) {
+       // console.log(msg);
+        setImagenes(img);
+      } else {
+        console.log("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  ///////// CAROUSEL - SLIDERS - IMGS
 
-    const deleteImg = async (path) => {
-        try {
-            const response = await axios.delete(`${BASE_URL}/upload/${path}`)
-            const img = await response.data.registros
-            const msg = await response.data.message
-            if (response.status === 200) {
-                console.log(msg)
-                setImagenes(img)
-            } else {
-                console.log('Ha ocurrido un error')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const uploadNewSlider = async (values) => {
+  //  console.log(`Context`, values);
+    try {
+      const response = await axios.post(`${BASE_URL}/upload/sliders`, values, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      });
+      const sliders = response.data.registros;
+      const message = response.data.message;
+      console.log(response);
+
+      if (response.status === 201) {
+        setImgsSlider(sliders);
+        //mensaje sweetalert2
+        agregado(message);
+      }
+
+      if (response.status === 304) {
+        //mensaje sweetalert2
+        agregado("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    ///////// CAROUSEL - SLIDERS - IMGS 
+  const updateSlader = async (file, path) => {
+  //  console.log("File", file, "Path", path);
+    const headers = {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    };
 
-    const uploadNewSlider = async (values) => {
-        console.log(`Context`, values)
-        try {
-            const response = await axios.post(`${BASE_URL}/upload/sliders`, values, {
-                headers: {
-                    'content-type': 'multipart/form-data'
-                }
-            })
-            const sliders = response.data.registros
-            const message = response.data.message
-            console.log(response)
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/upload/slider/${path}`,
+        file,
+        headers
+      );
+      const sliders = response.data.registros;
+      const message = response.data.message;
 
-            if (response.status === 201) {
-                setImgsSlider(sliders)
-                //mensaje sweetalert2
-                agregado(message)
-            }
+      if (response.status === 200) {
+        setImgsSlider(sliders);
+        //mensaje sweetalert2
+        agregado(message);
+      }
 
-            if (response.status === 304) {
-                //mensaje sweetalert2
-                agregado('Ha ocurrido un error')
-            }
-
-
-        } catch (error) {
-            console.log(error)
-        }
+      if (response.status === 304) {
+        //mensaje sweetalert2
+        agregado("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const deleteSlider = async (path) => {
+    try {
+      const response = await axios.delete(`${BASE_URL}/upload/slider/${path}`);
+      const slider = await response.data.registros;
 
-    const updateSlader = async (file, path) => {
-        console.log('File', file, 'Path', path)
-        const headers = {
-            headers: {
-                'content-type': 'multipart/form-data'
-            }
-        };
+      if (response.status === 200) {
+        setImgsSlider(slider);
+      }
 
-
-        try {
-            const response = await axios.put(`${BASE_URL}/upload/slider/${path}`, file, headers)
-            const sliders = response.data.registros
-            const message = response.data.message
-
-            if (response.status === 200) {
-                setImgsSlider(sliders)
-                //mensaje sweetalert2
-                agregado(message)
-            }
-
-            if (response.status === 304) {
-                //mensaje sweetalert2
-                agregado('Ha ocurrido un error')
-            }
-        } catch (error) {
-            console.log(error)
-        }
+      if (response.status === 304) {
+        //mensaje sweetalert2
+        agregado("Ha ocurrido un error");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  return (
+    <UserContext.Provider
+      value={{
+        userId,
+        setUserId,
+        proyects,
+        setProyects,
+        isUser,
+        setIsUser,
+        userType,
+        setUserType,
+        imagenes,
+        setImagenes,
+        loading,
+        uploadImg,
+        deleteProyectbyId,
+        deleteImg,
+        handleImg,
+        createProyect,
+        imgsSlider,
+        setImgsSlider,
+        uploadNewSlider,
+        deleteSlider,
+        updateImg,
+        updateSlader,
+        about,
+        logOut,
+        setAbout,
+        precios,
+        setPrecios,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
-
-
-    const deleteSlider = async path => {
-        try {
-            const response = await axios.delete(`${BASE_URL}/upload/slider/${path}`)
-            const slider = await response.data.registros
-
-            if (response.status === 200) {
-                setImgsSlider(slider)
-            }
-
-            if (response.status === 304) {
-                //mensaje sweetalert2
-                agregado('Ha ocurrido un error')
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-
-    return (
-        <UserContext.Provider
-            value={{
-                userId,
-                setUserId,
-                proyects,
-                setProyects,
-                isUser,
-                setIsUser,
-                userType,
-                setUserType,
-                imagenes,
-                setImagenes,
-                loading,
-                uploadImg,
-                deleteProyectbyId,
-                deleteImg,
-                handleImg,
-                createProyect,
-                imgsSlider,
-                setImgsSlider,
-                uploadNewSlider,
-                deleteSlider,
-                updateImg,
-                updateSlader,
-                about,
-                logOut,
-                setAbout,
-                precios,
-                setPrecios
-            }}
-        >
-
-            {children}
-        </UserContext.Provider>
-    )
-}
-
-export default UserContextProvider
+export default UserContextProvider;
--- src/Utils/URL.js
+++ src/Utils/URL.js
@@ -1,1 +1,3 @@
-export const BASE_URL = `https://backendportolio-production.up.railway.app`
+export const BASE_URL = `https://backendportolio-production.up.railway.app`;
+
+//export const BASE_URL = `http://localhost:3002`;
